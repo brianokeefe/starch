04091a613816f6a43aa4d161185074611a365e00
Brian O'Keefe <brian@bokstuff.com>
2014-07-26 22:39:36 -0400

    Add support for image optimization. Closes #3

--- skel/Gruntfile.js
+++ skel/Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: 'app',
-          src: 'assets/{files,fonts,images}/**/*',
+          src: 'assets/{files,fonts}/**/*',
           dest: '<%= config.stagingDir %>'
         }]
       },
@@ -38,7 +38,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: 'app',
-          src: 'assets/{files,fonts,images}/**/*',
+          src: 'assets/{files,fonts}/**/*',
           dest: '<%= config.buildDir %>'
         }]
       }
@@ -76,6 +76,24 @@ module.exports = function(grunt) {
           src: '**/*.html',
           dest: '<%= config.buildDir %>',
           ext: '.html'
+        }]
+      }
+    },
+    imagemin:{
+      staging: {
+        files: [{
+          expand: true,
+          cwd: 'app',
+          src: 'assets/images/**/*.{png,jpg,gif}',
+          dest: '<%= config.stagingDir %>'
+        }]
+      },
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'app',
+          src: 'assets/images/**/*.{png,jpg,gif}',
+          dest: '<%= config.buildDir %>'
         }]
       }
     },
@@ -131,6 +149,11 @@ module.exports = function(grunt) {
         tasks: ['generator:staging'],
         options: { livereload: true }
       },
+      images: {
+        files: 'app/assets/images/**/*.{png,jpg,gif}',
+        tasks: ['imagemin:staging'],
+        options: { livereload: true }
+      },
       js: {
         files: 'app/assets/js/**/*.js',
         tasks: ['uglify:staging'],
@@ -142,7 +165,7 @@ module.exports = function(grunt) {
         options: { livereload: true }
       },
       static: {
-        files: 'app/assets/{files,fonts,images}/**/*',
+        files: 'app/assets/{files,fonts}/**/*',
         tasks: ['copy:staging'],
         options: { livereload: true }
       }
@@ -152,6 +175,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
@@ -159,7 +183,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-generator');
 
   grunt.registerTask('serve', ['stage', 'express', 'watch']);
-  grunt.registerTask('stage', ['clean:staging', 'generator:staging', 'copy:staging', 'sass:staging', 'coffee:staging', 'uglify:staging']);
-  grunt.registerTask('build', ['clean:build', 'generator:build', 'copy:build', 'sass:build', 'coffee:build', 'uglify:build']);
+  grunt.registerTask('stage', ['clean:staging', 'generator:staging', 'copy:staging', 'imagemin:staging', 'sass:staging', 'coffee:staging', 'uglify:staging']);
+  grunt.registerTask('build', ['clean:build', 'generator:build', 'copy:build', 'imagemin:build', 'sass:build', 'coffee:build', 'uglify:build']);
 };
 
